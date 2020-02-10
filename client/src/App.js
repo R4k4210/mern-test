804ac82d8e895630b8cdedf4376dbbd820cdca37
Santiago Chopitea <elchopi26@gmail.com>
2020-02-10 02:53:02 -0300

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+import './App.css'
 
 class App extends Component {
   render () {
     return (
         <div className="App">
-          Hello World
+          <Login />
         </div>
     )
   }
