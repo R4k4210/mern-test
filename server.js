48d1d4b242d304d399ed83436518d07a59e2419e
Santiago Chopitea <schopitea@Movil02.intra.microgestion.com>
2020-04-03 03:03:38 -0300

    test4

--- server.js
+++ server.js
@@ -20,13 +20,10 @@ mongoose.connect(db,  {useUnifiedTopology: true, useNewUrlParser: true, useCreat
 // Use Routes
 app.use('/api/users', users);
 
-
 // Serve static assetts if in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    //app.use(express.static('client/build'));
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
-    
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
