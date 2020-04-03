681795670e8cf6e41e33eaae240f95b02154f937
Santiago Chopitea <schopitea@Movil02.intra.microgestion.com>
2020-04-03 02:17:50 -0300

    static

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.use('/api/items', items);
 // Serve static assetts if in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('client/build'));
+    //app.use(express.static('client/build'));
+    app.use('/static', express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
-        app.use('/static', express.static(path.join(__dirname, 'client/build')))
-        //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
