a3bc65e320d2d0c7515cb5b8ac8a7f97848804dd
Santiago Chopitea <schopitea@Movil02.intra.microgestion.com>
2020-04-03 01:44:13 -0300

    cambio en store

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -2,12 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import MainReducer from './reducers/MainReducer';
 import thunk from 'redux-thunk';
 
-//const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null;
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null;
 
 const Store = createStore(
     MainReducer,
     compose(applyMiddleware(thunk), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({latency: 0}))
+    devTools)
 );
 
 export default Store;
